1a293d428be128c617a245b24c2f6245d7c9b3c4
Prash <prash@pelucid.com>
2017-11-04 18:27:03 +0000

    add neuron functions

---
+++ feedforward/neuron.py
@@ -0,0 +1,27 @@
+
+import numpy as np
+
+np.random.seed(1)
+
+def sigmoid(x):
+    """Compute sigmoid function.
+
+    Defined as 1 / (1 + exp(-x))
+    """
+    return 1.0 / (1.0 + np.exp(-x))
+
+def sigmod_deriv(x):
+    """The Derivative can be expressed in terms
+    of the output of a sigmoid, s.
+
+    Args:
+        x
+
+    Returns ds, the derivative of the sigmoid wrt to x.
+    """
+#    if y.any() < 0.0 or y.any() > 1.0:
+#        raise Exception
+
+    s = sigmoid(x)
+    ds = s*(1-s)
+    return ds
